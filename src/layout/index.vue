0a05da959f276936c217780ec8231c1465d4a0b3
zhangm <maih1314>
2021-06-25 18:06:20 +0800

    add some layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,141 +1,296 @@
 <template>
-  <n-space vertical>
-    <!-- <n-space> <n-switch v-model:value="inverted" /> inverted </n-space> -->
-    <n-layout style='width:100%;height:100%'>
-      <n-layout-header :inverted="inverted" bordered>
-        Header Header Header
-        <n-menu mode="horizontal" :inverted="inverted" :options="menuOptions" />
+    <n-layout class="w-h-100">
+      <n-layout-header :inverted="inverted" bordered style="background-color: #e7f5ee">
+        <div class="header-container">
+          zmheang's zone
+        </div>
+        <!--        <n-menu mode="horizontal" :inverted="inverted" :options="menuOptions" />-->
       </n-layout-header>
-      <n-layout has-sider class="main-containor">
+      <n-layout has-sider position="absolute" style="top: 50px; bottom: 0px;">
         <n-layout-sider
-          bordered
-          show-trigger
-          collapse-mode="width"
-          :collapsed-width="64"
-          :width="240"
-          :native-scrollbar="false"
-          :inverted="inverted"
-          style="max-height: 320px"
+                bordered
+                show-trigger
+                collapse-mode="width"
+                :collapsed-width="64"
+                :width="240"
+                :inverted="inverted"
+                :native-scrollbar="false"
         >
           <n-menu
-            :inverted="inverted"
-            :collapsed-width="64"
-            :collapsed-icon-size="22"
-            :options="menuOptions"
+                  :inverted="inverted"
+                  :collapsed-width="64"
+                  :collapsed-icon-size="22"
+                  :options="menuOptions"
           />
         </n-layout-sider>
-        <n-layout style="max-height: 100%">
+        <n-layout>
           <router-view></router-view>
         </n-layout>
       </n-layout>
-      <n-layout-footer :inverted="inverted" bordered>
-        Footer Footer Footer
-      </n-layout-footer>
     </n-layout>
-  </n-space>
+
+
 </template>
 
 <script>
-import { h, defineComponent, ref } from "vue";
-import { NIcon } from "naive-ui";
-import {
-  BookOutline as BookIcon,
-  PersonOutline as PersonIcon,
-  WineOutline as WineIcon,
-} from "@vicons/ionicons5";
+  import {h, defineComponent, ref} from "vue";
+  import {NIcon} from "naive-ui";
+  import {
+    BookOutline as BookIcon,
+    CloudyNight as CloudyNightIcon,
+    Cloud as CloudIcon,
+    LogoSoundcloud as LogoSoundcloudIcon,
+    Woman as WomanIcon,
+    Flag as FlagIcon,
+    Bonfire as BonfireIcon,
+    AirplaneSharp as AirplaneSharpIcon,
+    LeafSharp as LeafSharpIcon,
+    Water as WaterIcon,
+    Heart as HeartIcon,
+    Flower as FlowerIcon,
+    Fish as FishIcon,
+    Moon as MoonIcon,
+    VolumeMedium as VolumeMediumIcon,
+  } from "@vicons/ionicons5";
 
-function renderIcon(icon) {
-  return () => h(NIcon, null, { default: () => h(icon) });
-}
+  function renderIcon(icon) {
+    return () => h(NIcon, null, {default: () => h(icon)});
+  }
 
-const menuOptions = [
-  {
-    label: "且听风吟",
-    key: "hear-the-wind-sing",
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: "1973年的弹珠玩具",
-    key: "pinball-1973",
-    icon: renderIcon(BookIcon),
-    disabled: true,
-    children: [
-      {
-        label: "鼠",
-        key: "rat",
-      },
-    ],
-  },
-  {
-    label: "寻羊冒险记",
-    key: "a-wild-sheep-chase",
-    disabled: true,
-    icon: renderIcon(BookIcon),
-  },
-  {
-    label: "舞，舞，舞",
-    key: "dance-dance-dance",
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        type: "group",
-        label: "人物",
-        key: "people",
-        children: [
-          {
-            label: "叙事者",
-            key: "narrator",
-            icon: renderIcon(PersonIcon),
-          },
-          {
-            label: "羊男",
-            key: "sheep-man",
-            icon: renderIcon(PersonIcon),
-          },
-        ],
-      },
-      {
-        label: "饮品",
-        key: "beverage",
-        icon: renderIcon(WineIcon),
-        children: [
-          {
-            label: "威士忌",
-            key: "whisky",
-          },
-        ],
-      },
-      {
-        label: "食物",
-        key: "food",
-        children: [
-          {
-            label: "三明治",
-            key: "sandwich",
-          },
-        ],
-      },
-      {
-        label: "过去增多，未来减少",
-        key: "the-past-increases-the-future-recedes",
-      },
-    ],
-  },
-];
+  const menuOptions = [
+    {
+      label: "教我如何不想她?",
+      key: "menu-1",
+      icon: renderIcon(BookIcon),
+      disabled: false,
+      children: [
+        {
+          label: "天上飘着些微云",
+          icon: renderIcon(CloudIcon),
+          key: "menu-1-1",
+        },
+        {
+          label: "地上吹着些微风",
+          icon: renderIcon(FlagIcon),
+          key: "menu-1-2",
+        },
+        {
+          label: "啊",
+          icon: renderIcon(VolumeMediumIcon),
+          key: "menu-1-3",
+        },
+        {
+          label: "微风吹动了我头发",
+          icon: renderIcon(WomanIcon),
+          key: "menu-1-4",
+        },
+        {
+          label: "教我如何不想她",
+          icon: renderIcon(HeartIcon),
+          key: "menu-1-5",
+        },
+        {
+          label: "月光恋爱着海洋",
+          icon: renderIcon(MoonIcon),
+          key: "menu-1-6",
+        },
+        {
+          label: "海洋恋爱着月光",
+          icon: renderIcon(WaterIcon),
+          key: "menu-1-7",
+        },
+        {
+          label: "啊",
+          icon: renderIcon(VolumeMediumIcon),
+          key: "menu-1-8",
+        },
+        {
+          label: "这般蜜也似的银夜",
+          icon: renderIcon(CloudyNightIcon),
+          key: "menu-1-9",
+        },
+        {
+          label: "教我如何不想她",
+          icon: renderIcon(HeartIcon),
+          key: "menu-1-10",
+        },
+        {
+          label: "水面落花慢慢流",
+          icon: renderIcon(FlowerIcon),
+          key: "menu-1-11",
+        },
+        {
+          label: "水底鱼儿慢慢游",
+          icon: renderIcon(FishIcon),
+          key: "menu-1-12",
+        },
+        {
+          label: "啊",
+          icon: renderIcon(VolumeMediumIcon),
+          key: "menu-1-13",
+        },
+        {
+          label: "燕子你说些什么话",
+          icon: renderIcon(AirplaneSharpIcon),
 
-export default defineComponent({
-  setup() {
-    return {
-      inverted: ref(false),
-      menuOptions,
-    };
-  },
-});
+          key: "menu-1-14",
+        },
+        {
+          label: "教我如何不想她",
+          icon: renderIcon(HeartIcon),
+          key: "menu-1-15",
+        },
+        {
+          label: "枯树在冷风里摇",
+          icon: renderIcon(LeafSharpIcon),
+          key: "menu-1-16",
+        },
+        {
+          label: "野火在暮色中烧",
+          icon: renderIcon(BonfireIcon),
+          key: "menu-1-17",
+        },
+        {
+          label: "啊",
+          icon: renderIcon(VolumeMediumIcon),
+          key: "menu-1-18",
+        },
+        {
+          label: "西天还有些儿残霞",
+          icon: renderIcon(LogoSoundcloudIcon),
+          key: "menu-1-19",
+        },
+        {
+          label: "教我如何不想她",
+          icon: renderIcon(HeartIcon),
+          key: "menu-1-20",
+        }
+      ],
+    },
+    {
+      label: "断章",
+      key: "menu-2",
+      icon: renderIcon(BookIcon),
+      disabled: false,
+      children: [
+        {
+          label: "你站在桥上看风景",
+          key: "menu-2-1",
+        },
+        {
+          label: "看风景的人在楼上看你",
+          key: "menu-2-2",
+        },
+        {
+          label: "明月装饰了你的窗子",
+          key: "menu-2-3",
+        },
+        {
+          label: "你装饰了别人的梦",
+          key: "menu-2-4",
+        },
+      ],
+    },
+    {
+      label: "从前慢",
+      key: "menu-3",
+      icon: renderIcon(BookIcon),
+      children: [
+        {
+          label: "记得早先少年时",
+          key: "menu-3-1",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "大家诚诚恳恳",
+          key: "menu-3-2",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "说一句，是一句",
+          key: "menu-3-3",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "清早上火车站",
+          key: "menu-3-4",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "长街黑暗无行人",
+          key: "menu-3-5",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "卖豆浆的小店冒着热气",
+          key: "menu-3-6",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "从前的日色变得慢",
+          key: "menu-3-7",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "车，马，邮件都慢",
+          key: "menu-3-8",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "一生只够爱一个人",
+          key: "menu-3-9",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "从前的锁也好看",
+          key: "menu-3-10",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "钥匙精美有样子",
+          key: "menu-3-11",
+          icon: renderIcon(BookIcon)
+        },
+        {
+          label: "你锁了，人家就懂了",
+          key: "menu-3-12",
+          icon: renderIcon(BookIcon)
+        }
+
+      ]
+    }
+  ];
+
+  export default defineComponent({
+    setup() {
+      return {
+        inverted: ref(false),
+        menuOptions,
+      };
+    }
+  });
 </script>
 
 <style lang="scss" scoped>
-.main-containor {
-  width: 100%;
-  height: calc(100vh - 74px);
-}
+  $header-height: 50px;
+  $footer-height: 50px;
+
+
+  .header-container {
+    height: $header-height;
+    line-height: $header-height;
+    padding-left: 30px;
+    font-size: 22px;
+    font-weight: 600;
+  }
+
+  .main-container {
+    height: calc(100% - #{$header-height + $footer-height});
+  }
+
+  .foot-container {
+    height: $footer-height;
+  }
+
+
 </style>
